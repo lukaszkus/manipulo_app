43ea18c3ee3cf423dcef73b4b6a24ee4ce6425c2
lukaszkus <kusiu@vp.pl>
2020-10-14 00:20:30 +0200

    Welcome layout updated

--- src/layouts/Welcome/Welcome.js
+++ src/layouts/Welcome/Welcome.js
@@ -3,26 +3,26 @@ import './Welcome.scss';
 import logo from '../../assets/logo_mb.svg';
 
 function Welcome() {
-    return (
-      <div id="welcome">
-        <div className="grid__welcome container">
-        <div className="header"></div>
-
-        <div className="main animate">
-          <div className="main__heading">
-            <h1>Witaj</h1>
-            <p>Skonfiguruj i zamów swoją tablicę manipulacyjną.</p>
+  return (
+      <div className="welcome">
+          <div className="container">
+          <div className="grid-template-welcome">
+              <div className="header"></div>
+              <div className="main-left animate">
+                <div className="main-left__heading">
+                  <h1>Witaj</h1>
+                  <p>Skonfiguruj i zamów swoją tablicę manipulacyjną.</p>
+                </div>
+                <button>ROZPOCZNIJ</button>
+              </div>
+              <div className="footer animate">
+                <div className="copyright">
+                  <p>Copyright &copy;</p>
+                  <a href="https://www.manipuloboards.pl"><img src={logo} alt="Logo" className="footer__logo"/></a>
+                </div>
+              </div>
           </div>
-          <button>ROZPOCZNIJ</button>
-        </div>
-
-        <div className="footer animate">
-          <div className="copyright">
-              <p>Copyright &copy;</p>
-              <a href="https://www.manipuloboards.pl"><img src={logo} alt="Logo" className="footer__logo"/></a>
           </div>
-        </div>
-         </div>
       </div>
     );
   }
