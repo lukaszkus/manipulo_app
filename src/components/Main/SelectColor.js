e5add6a17bde4596a0e2cd73bb74e2a369cfd482
lukaszkus <kusiu@vp.pl>
2020-11-05 20:38:45 +0100

    updated SelectColor component

--- src/components/Main/SelectColor.js
+++ src/components/Main/SelectColor.js
@@ -1,91 +1,33 @@
 import React from 'react'
 import './SelectColor.scss'
+import { colors } from './board_colors.js';
 
 function SelectColor({ onSelection, setPrice }) {
+
     return (
         <div className="animate">
             <div className="selectColor">
                 <div className="selectColor__container">
-                    <p className="color-prices">biały - 45 zł</p>
-                    <div className="boxes">
-                        <div className="selectColor__box">
-                            <div className="color-box white"
-                                onClick={() => {
-                                    onSelection("biały");
-                                    setPrice("45");
-                                }}>
-                            </div>
-                            <p className="color-name">biały</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="selectColor__container">
-                    <p className="color-prices">kolor - 75 zł</p>
-                    <div className="boxes">
-                        <div className="selectColor__box">
-                            <div className="color-box light-grey"
-                                onClick={() => {
-                                    onSelection("jasny szary")
-                                    setPrice("75");
-                                }}>
-                            </div>
-                            <p className="color-name">jasny szary</p>
-                        </div>
-                        <div className="selectColor__box">
-                            <div className="color-box dark-grey"
-                                onClick={() => {
-                                    onSelection("grafit")
-                                    setPrice("75");
-                                }}>
-                            </div>
-                            <p className="color-name">grafit</p>
-                        </div>
-                        <div className="selectColor__box">
-                            <div className="color-box black"
-                                onClick={() => {
-                                    onSelection("czarny")
-                                    setPrice("75");
-                                }}>
-                            </div>
-                            <p className="color-name">czarny</p>
-                        </div>
-                        <div className="selectColor__box">
-                            <div className="color-box light-blue"
-                                onClick={() => {
-                                    onSelection("niebieski jasny")
-                                    setPrice("75");
-                                }}>
-                            </div>
-                            <p className="color-name">niebieski jasny</p>
-                        </div>
-                        <div className="selectColor__box">
-                            <div className="color-box blue"
-                                onClick={() => {
-                                    onSelection("niebieski")
-                                    setPrice("75");
-                                }}>
-                            </div>
-                            <p className="color-name">niebieski</p>
-                        </div>
-                        <div className="selectColor__box">
-                            <div className="color-box pink"
-                                onClick={() => {
-                                    onSelection("róż")
-                                    setPrice("75");
-                                }}>
-                            </div>
-                            <p className="color-name">róż</p>
-                        </div>
-                        <div className="selectColor__box">
-                            <div className="color-box red"
-                                onClick={() => {
-                                    onSelection("czerwony")
-                                    setPrice("75");
-                                }}>
-                            </div>
-                            <p className="color-name">czerwony</p>
-                        </div>
-                    </div>
+                    {/* <p className="color-prices">Biały - 45 zł</p> */}
+
+                    <ul className="colors__list">
+                        {colors.map((el, id) => {
+                            return (
+                                <li key={id}
+                                    onClick={() => {
+                                        setPrice(`${el.price}`);
+                                        onSelection(`${el.name}`);
+                                    }}>
+                                    <div className="color-box">
+                                        <input type="radio" id={id} value={el.price} name="color" />
+                                        <label htmlFor={id}><div className="box" style={{ backgroundColor: `${el.color}` }}></div></label>
+                                    </div>
+                                    <p className="color-name">{el.name}</p>
+                                </li>
+                            );
+                        })}
+                    </ul>
+
                 </div>
             </div>
         </div>
