39925d556cdd39415e517cce13723981174b2dfb
lukaszkus <kusiu@vp.pl>
2020-11-05 20:39:46 +0100

    updated SelectElements Component

--- src/components/Main/SelectElements.js
+++ src/components/Main/SelectElements.js
@@ -6,22 +6,23 @@ function SelectElements({ setSelectedElements, setPrice }) {
   return (
     <div className="animate">
       <div className="elements__container">
-        {elements.map((el, id) => {
-          let tooltip = <span>{el.price} zł</span>;
-          return (
-            <div key={id} className="element"
-              onClick={() => {
-                setSelectedElements(el.name);
-                setPrice(el.price);
-              }}>
-              <div className="element__box text">
-                <span className="tooltip">{tooltip}</span>
-                <img src={el.url} alt={el.name} />
-              </div>
-              <p className="element__name">{el.name}</p>
-            </div>
-          );
-        })}
+        <ul className="elements__list">
+          {elements.map((el, id) => {
+            return (
+              <li key={id} className="element"
+                onClick={() => {
+                  setSelectedElements(el.name);
+                  setPrice(el.price);
+                }}>
+                <div className="box">
+                  <input type="checkbox" id={id} />
+                  <label htmlFor={id}><img src={el.url} alt={el.name} /></label>
+                </div>
+                <p className="element__name">{el.name}</p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   )
