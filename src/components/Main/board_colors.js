2e55a14a888b1c2514546c4285812fa92f55b94f
lukaszkus <kusiu@vp.pl>
2020-11-05 20:52:01 +0100

    create board_colors.js

---
+++ src/components/Main/board_colors.js
@@ -0,0 +1,50 @@
+export const colors = [
+    {
+        "id": 1,
+        "name": "biały",
+        "color": "white",
+        "price": 45,
+    },
+    {
+        "id": 2,
+        "name": "jasny szary",
+        "color": "lightgrey",
+        "price": 75,
+    },
+    {
+        "id": 3,
+        "name": "grafit",
+        "color": "darkgrey",
+        "price": 75,
+    },
+    {
+        "id": 4,
+        "name": "czarny",
+        "color": "black",
+        "price": 75,
+    },
+    {
+        "id": 5,
+        "name": "niebieski jasny",
+        "color": "lightblue",
+        "price": 75,
+    },
+    {
+        "id": 6,
+        "name": "niebieski",
+        "color": "blue",
+        "price": 75,
+    },
+    {
+        "id": 7,
+        "name": "róż",
+        "color": "pink",
+        "price": 75,
+    },
+    {
+        "id": 8,
+        "name": "czerwony",
+        "color": "red",
+        "price": 75,
+    }
+]
