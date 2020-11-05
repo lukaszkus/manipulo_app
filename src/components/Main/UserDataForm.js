08a59e74a63da28a4ca2a830ba9596fd36bc7da3
lukaszkus <kusiu@vp.pl>
2020-11-05 20:54:02 +0100

    UserDataForm component create

--- src/components/Main/UserDataForm.js
+++ src/components/Main/UserDataForm.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import '../../components/Main/UserDataForm.scss'
 
 function UserDataForm() {
     return (
-        <div>
-            <h1>Twoje dane</h1>
+        <div className="animate">
+            <div className="userDF">
+                <div className="userDF__container">
+                    <p className="userDF__title">Podaj dane do wysyłki</p>
+                    <form>
+                        <label>
+                            Imię i nazwisko
+                            <input type="text" />
+                        </label>
+                        <label>
+                            Ulica i numer domu
+                            <input type="text"  />
+                        </label>
+                        <label>
+                            Kod pocztowy
+                            <input type="text" />
+                        </label>
+                        <label>
+                            Miejscowość
+                            <input type="text" />
+                        </label>
+                        <label>
+                            Adres e-mail
+                            <input type="text" />
+                        </label>
+                        <label>
+                            Nr telefonu
+                            <input type="text" />
+                        </label>
+                    </form>
+                </div>
+                <div className="selectOther__container">
+
+                </div>
+            </div>
         </div>
     )
 }
