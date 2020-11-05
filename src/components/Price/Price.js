585a2c58eb142151e63137b2dad9c92a610d5646
lukaszkus <kusiu@vp.pl>
2020-11-05 20:55:07 +0100

    update Price component

--- src/components/Price/Price.js
+++ src/components/Price/Price.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Price.scss'
 
 function Price({ price }) {
     return (
-        <div>
+        <div className="price__container">
             <h4>Cena tablicy:</h4>
             <h1>{price} zł</h1>
         </div>
